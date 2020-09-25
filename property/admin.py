f2a056ba9a5a2e869279c388e5ef8ca00a82420b
herypank <60302361+herypank@users.noreply.github.com>
2020-09-25 11:57:44 +0300

    Поправил вывод полей, сделал new_building изменяемым.

--- property/admin.py
+++ property/admin.py
@@ -7,4 +7,6 @@ from property.models import Flat
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address', 'owner',]
     readonly_fields = ['created_at',]
+    list_display  = ['town', 'new_building', "address", "price"]
+    list_editable = ['new_building',]
 admin.site.register(Flat, FlatAdmin)
