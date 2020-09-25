b78e08de635d7d0b7e9667555190676e8b754c7f
herypank <60302361+herypank@users.noreply.github.com>
2020-09-25 11:06:13 +0300

    Добавил поиск, обновил gitignore.

--- property/admin.py
+++ property/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
 from .models import Flat
-
-
-admin.site.register(Flat)
+from property.models import Flat
+ 
+ 
+class FlatAdmin(admin.ModelAdmin):
+    search_fields = ('town', 'address', 'owner',)
+ 
+admin.site.register(Flat, FlatAdmin)
