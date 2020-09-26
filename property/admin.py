3b3aa260e7885f4e3c53a400d4f8e8aa246a4c7d
herypank <60302361+herypank@users.noreply.github.com>
2020-09-27 02:23:17 +0300

    pep8 admin.py

--- property/admin.py
+++ property/admin.py
@@ -1,26 +1,26 @@
 from django.contrib import admin
 
-from .models import Flat
 from property.models import Flat, Appeal, Owner
- 
- 
+
+
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ['town', 'address', 'owner',]
-    readonly_fields = ['created_at',]
-    list_display  = ["address", 'price', 'new_building', "construction_year", 'town',]
-    list_editable = ['new_building',]
-    list_filter = ['new_building', 'rooms_number', 'has_balcony',]
-    raw_id_fields = ["who_liked"]
+    search_fields = ['town', 'address', 'owner', ]
+    readonly_fields = ['created_at', ]
+    list_display = [
+        "address", 'price', 'new_building', "construction_year", 'town', ]
+    list_editable = ['new_building', ]
+    list_filter = ['new_building', 'rooms_number', 'has_balcony', ]
+    raw_id_fields = ["who_liked", ]
+
 
 class AppealAdmin(admin.ModelAdmin):
     raw_id_fields = ("apartment", 'author')
     list_display = ["author", "apartment"]
-    
+
 
 class OwnerAdmin(admin.ModelAdmin):
-    raw_id_fields = ["owned_apartments",]
-    list_display = ["owner", "owner_pure_phone"]
-
+    raw_id_fields = ["owned_apartments", ]
+    list_display = ["owner", "owner_pure_phone", ]
 
 
 admin.site.register(Flat, FlatAdmin)
