8aca687671e8b57e62fc4e9cb3bc22cc449be052
herypank <60302361+herypank@users.noreply.github.com>
2020-09-27 02:28:44 +0300

    pep8 views.py

--- property/views.py
+++ property/views.py
@@ -28,9 +28,8 @@ def show_flats(request):
         flats = flats.exclude(price=None)
     if new_building:
         flats = flats.filter(new_building=new_building)
-
-
-    towns = Flat.objects.values_list('town', flat=True).distinct().order_by('town')
+    towns = Flat.objects.values_list(
+        'town', flat=True).distinct().order_by('town')
     return render(request, 'flats_list.html', {
         'flats': flats[:10],
         'towns': towns,
